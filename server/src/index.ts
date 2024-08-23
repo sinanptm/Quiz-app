8decfe3f838bcbaeb8ab7bbd01cb7c84fce6fb1a
Sinan <muhammedsinan7749@gmail.com>
2024-08-23 06:47:58 +0530

    cors origin updated

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "https://quiz-app-topaz-beta-82.vercel.app/",
+  origin: "https://quiz-app-six-dusky.vercel.app/",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", 'HEAD', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
   credentials: true,
