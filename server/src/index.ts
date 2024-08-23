e9f24e2a8a3552f05d7d9d968b03e3bc2fd6970a
Sinan <muhammedsinan7749@gmail.com>
2024-08-23 06:17:05 +0530

    cors updated for production

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,7 +12,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: '*', 
+  origin: "https://quiz-app-topaz-beta-82.vercel.app/",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 };
 
 app.use(cookieParser());
