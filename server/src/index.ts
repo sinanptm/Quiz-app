86b9640165a726bf61f24ad49db104c575cbaaec
Sinan <muhammedsinan7749@gmail.com>
2024-08-23 07:37:23 +0530

    hello page added

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "https://quiz-app-topaz-beta-82.vercel.app/",
+  origin: "quiz-p9d71zl0z-sinans-projects-8d312afe.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
@@ -23,12 +23,12 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-  res.send('hello')
+app.get('/hello',(req,res)=>{
+  res.send("Hello this is a sample page");
 })
 
 app.use("/questions", quizRoutes);
-// app.use("/", adminRoutes);
+app.use("/", adminRoutes);
 
 connectDB().then(() => {
   app.listen(PORT, () => {
